5f4562ea6eead297672f9050a7ea8475a411a59e
Jesus Guerra Rosas <Guerrix@Elisa.local>
2012-03-19 11:57:29 -0700

    Set torch off when the app enter to background

--- TorchLED4/TorchLED4AppDelegate.m
+++ TorchLED4/TorchLED4AppDelegate.m
@@ -26,7 +26,24 @@
     return YES;
 }
 
-
+-(void)setOffTorch{
+    
+    if ( self.viewController.isTorch) {
+        self.viewController.imageBackground.hidden = YES;
+        self.viewController.isTorch=FALSE;
+        
+        UIImage *image = [UIImage imageNamed:@"turnedOffButton.png"];
+        UIImage *powerOff = [image stretchableImageWithLeftCapWidth:0 topCapHeight:0.0];
+        
+        [self.viewController.powerButton setBackgroundImage:powerOff forState:UIControlStateNormal];
+        image = nil;
+        [image release];
+        
+    }
+    
+    
+    
+}
 
 - (void)applicationWillResignActive:(UIApplication *)application
 {
@@ -34,7 +51,7 @@
      Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
      Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
      */
-   
+    [self setOffTorch];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
@@ -43,7 +60,7 @@
      Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
      If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
      */
-
+    [self setOffTorch];
 }
 
 
